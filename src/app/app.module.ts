d477c549aa11a3eaa08afd83789451f24bfed4b9
BraianBecha <braian.becha.hace@gmail.com>
2022-07-23 00:27:49 -0300

    i primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PortadaComponent } from './portada/portada.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PortadaComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/portada/portada.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PortadaComponent } from './portada.component';
+
+describe('PortadaComponent', () => {
+  let component: PortadaComponent;
+  let fixture: ComponentFixture<PortadaComponent>;
+
+  function cambiar_parrafo(){
+
+};
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PortadaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PortadaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  
+});
